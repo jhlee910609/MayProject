db0aeafaf5402a08eed552ee59be5462bc2ef95a
JunHee Lee <jhlee910609@gmail.com>
2017-06-05 13:09:40 +0900

    ====== modifiying code ======= 1. add googleMap. 2. add 'README' file. 3. add code Runtime permission check.

--- app/src/main/java/com/junhee/android/mayproject/MainActivity.java
+++ app/src/main/java/com/junhee/android/mayproject/MainActivity.java
@@ -1,12 +1,20 @@
 package com.junhee.android.mayproject;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,11 +25,21 @@ public class MainActivity extends AppCompatActivity {
     ViewPager pager;
     Fragment gallery, cal, webViewer, unitChanger, map;
     List<Fragment> datas;
+    LocationManager manager;
+
+    public LocationManager getLocationManager() {
+        return manager;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Context.를 달아주는 이유는 현재 컨텍스트의.LocationSERVIE를 명시해줘야하기 때문에
+
+        // 프레그먼트 생성주기가 변화하지만 매니져는 액티비티에서 계속 생성되어 있기 때문에 새로운 프로그먼트들이
+        // 해당 매니져를 사용하는데에 전혀 지장이 없음
+        manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         // 1. 탭 구성
         pager = (ViewPager) findViewById(R.id.pager);
@@ -56,8 +74,47 @@ public class MainActivity extends AppCompatActivity {
         // 5. 페이지 및 탭 넘길 때, 같이 넘어가도록 설정
         pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tab));
         tab.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(pager));
+
+
+        // 퍼미션을 위한 버전체크
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermission();
+        }
+    }
+
+    // 위치제공자 사용을 위한 권한처리
+    private final int REQ_PERMISSION = 100;
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkPermission(){
+        //1 권한체크 - 특정권한이 있는지 시스템에 물어본다
+        if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+
+        }else{
+            // 2. 권한이 없으면 사용자에 권한을 달라고 요청
+            String permissions[] = {Manifest.permission.ACCESS_FINE_LOCATION};
+            requestPermissions(permissions ,REQ_PERMISSION); // -> 권한을 요구하는 팝업이 사용자 화면에 노출된다
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQ_PERMISSION){
+            // 3.1 사용자가 승인을 했음
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            }else{
+                cancel();
+            }
+        }
+    }
+
+    public void cancel(){
+        Toast.makeText(this, "권한요청을 승인하셔야 GPS를 사용할 수 있습니다.", Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
+
 
 // 페이저 어댑터 setting
 class PagerAdapter extends FragmentStatePagerAdapter {
--- app/src/main/java/com/junhee/android/mayproject/MapFrag.java
+++ app/src/main/java/com/junhee/android/mayproject/MapFrag.java
@@ -1,7 +1,14 @@
 package com.junhee.android.mayproject;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,13 +22,13 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import static com.junhee.android.mayproject.R.id.map;
-
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MapFrag extends Fragment implements OnMapReadyCallback {
 
+    LocationManager manager;
+    GoogleMap map = null;
 
     public MapFrag() {
         // Required empty public constructor
@@ -32,16 +39,22 @@ public class MapFrag extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_map, container, false);
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(map);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // 상위 택티비티의 자원을 사용하기 위해서 Activity를 가져옴
+        // 나를 호출한 액티비티 호출
+        MainActivity activity = (MainActivity) getActivity();
+
+        manager = activity.getLocationManager();
 
         return view;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        map = googleMap;
         // 좌표만 생성
         LatLng home = new LatLng(37.321362, 127.087111);
         // 좌표를 적용
@@ -49,8 +62,86 @@ public class MapFrag extends Fragment implements OnMapReadyCallback {
         // 1. 마커를 화면에 그린다
         MarkerOptions marker = new MarkerOptions().position(home);
         marker.title("우리 집");
-        googleMap.addMarker(marker);
+        map.addMarker(marker);
         // 2. 앱의 중심을 해당 좌표로 이동시킨다. >                   // 좌표 // 줌 레벨
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 10));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 10));
     }
+    // 현재 프레그먼트가 러닝직전
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // 마시멜로 이상 버전에서는 런타임 권한 체크여부를 확인해야 함
+
+        // 마시멜로 이상버전에서는 런타임 권한 체크여부를 확인해야 한다.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // GPS 사용을 위한 권한 획득이 되어 있지 않으면 리스너 등록을 하지 않는다
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+        }
+        // GPS 리스너 등록
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 위치제공자
+                3000, // 변경사항 체크 주기 millisecond
+                1,    // 변경사항 체크 거리 meter
+                locationListener
+        );
+    }
+
+    // 현재 프레그먼트가 정지
+    @Override
+    public void onPause() {
+        super.onPause();
+        // 마시멜로 이상버전에서는 런타임 권한 체크여부를 확인해야 한다.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // GPS 사용을 위한 권한 획득이 되어 있지 않으면 리스너를 해제하지 않는다.
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+        }
+        // 리스너 해제
+        manager.removeUpdates(locationListener);
+    }
+
+    // GPS를 사용하기 위해서 좌표 리스너를 생성
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            // 경도
+            double lng = location.getLongitude();
+            // 위도
+            double lat = location.getLatitude();
+            // 고도
+            double alt = location.getAltitude();
+            // 정확도
+            float acc = location.getAccuracy();
+            // 위치 제공자
+            String provider = location.getProvider();
+            // 바뀐 현재 좌표를 넣어줌
+            LatLng current = new LatLng(lat, lng);
+
+            // 현재 좌표로 카메라로 이동
+            // 구글맵 전역으로 뺌
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 17));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 }
+
+
